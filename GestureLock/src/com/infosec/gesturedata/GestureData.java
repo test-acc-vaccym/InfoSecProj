08af1ceab1ef7c3b9bd7135f01cde5b38e5eb7a3
Bryan Lutz <bryanlutz.212@gmail.com>
2014-04-01 06:48:58 -0400

    Added methods to GestureData Created Point Class for origin and ending point

--- GestureLock/src/com/infosec/gesturedata/GestureData.java
+++ GestureLock/src/com/infosec/gesturedata/GestureData.java
@@ -4,11 +4,43 @@ import java.util.ArrayList;
 public class GestureData {
 
 	private ArrayList<AccelEvent> data;
+	private Point finalPosition;
 
 	public GestureData(ArrayList<AccelEvent> data) {
 		this.data = data;
-		//for initial commit
 	}
-	
-	
+
+	/* Returns a point that represents the final ending 
+	 * position after all data is parsed
+	 */
+	public Point getPosition() {
+		return this.finalPosition;
+	}
+
+	/* Returns an array list of points where each point is
+	 * the average of one time interval
+	 */
+	private ArrayList<Point> getAverages() {
+		ArrayList<Point> averages = new ArrayList<Point>();
+
+		// TODO
+		// This method should call calcAverageOfInterval(start, end)
+
+		return averages;
+	}
+
+	/* Returns a point that represents the averages
+	 * @args start Starting time in nanoseconds
+	 * @args end End of the interval in nanaseconds
+	 */
+	private Point calcAverageOfInterval(long start, long end) {
+		Point p = new Point();
+
+		// TODO
+
+		return p;
+	}
+
+
+
 }
